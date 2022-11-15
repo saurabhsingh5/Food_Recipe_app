25d74b2ba0a667f43cf5876edfff3109178d405f
SAURABH SINGH <justusingh73@gmail.com>
2022-11-15 18:05:53 +0530

    Updated Recipe.jsx

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,29 +1,28 @@
 import { useEffect, useState } from "react";
-import styled from 'styled-components';
-import {useParams} from 'react-router-dom';
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
 
-
-import React from 'react'
+import React from "react";
 
 function Recipe() {
-
   let params = useParams();
 
   const [details, setDetails] = useState({});
-  const [activeTab, setActiveTab] = useState('instructions');
+  const [activeTab, setActiveTab] = useState("instructions");
 
-  const fetchDetails = async () => {
-    const data = await fetch(
-      `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`);
-    const detailData = await data.json();
-    setDetails(detailData);
-    // console.log(detailData);
-  }
+ 
 
   useEffect(() => {
+    const fetchDetails = async () => {
+      const data = await fetch(
+        `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+      );
+      const detailData = await data.json();
+      setDetails(detailData);
+    };
     fetchDetails();
   }, [params.name]);
-  
+
   return (
     <DetailWrapper>
       <div>
@@ -31,57 +30,55 @@ function Recipe() {
         <img src={details.image} alt="" />
       </div>
       <Info>
-        <Button 
-            className={activeTab === 'instructions' ? 'active' : ''} 
-            onClick={() => setActiveTab("instructions")}
-            >
-              Instructions
+        <Button
+          className={activeTab === "instructions" ? "active" : ""}
+          onClick={() => setActiveTab("instructions")}
+        >
+          Instructions
         </Button>
-        <Button 
-            className={activeTab === 'ingredients' ? 'active' : ''} 
-            onClick={() => setActiveTab("ingredients")}
-            >
-              Ingredients
+        <Button
+          className={activeTab === "ingredients" ? "active" : ""}
+          onClick={() => setActiveTab("ingredients")}
+        >
+          Ingredients
         </Button>
-        {activeTab === 'instructions' && (
+        {activeTab === "instructions" && (
           <div>
-            <h3 dangerouslySetInnerHTML={{ __html:details.summary }}></h3>
-            <h3 dangerouslySetInnerHTML={{ __html:details.instructions }}></h3>
+            <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+            <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
           </div>
         )}
-        {activeTab === 'ingredients' && (
+        {activeTab === "ingredients" && (
           <ul>
-          {details.extendedIngredients.map((ingredient) =>(
-            <li key={ingredient.id}>{ingredient.original}</li>
-          ))}
-        </ul>
+            {details.extendedIngredients.map((ingredient) => (
+              <li key={ingredient.id}>{ingredient.original}</li>
+            ))}
+          </ul>
         )}
-        
       </Info>
     </DetailWrapper>
-  )
+  );
 }
-
 
 const DetailWrapper = styled.div`
   margin-top: 10rem;
   margin-bottom: 5rem;
   display: flex;
-  .active{
+  .active {
     background: linear-gradient(35deg, #494949, #313131);
     color: white;
   }
-  h2{
+  h2 {
     margin-bottom: 5rem;
   }
-  li{
+  li {
     font-size: 1.2rem;
     line-height: 2.5rem;
   }
-  ul{
+  ul {
     margin-top: 2rem;
   }
-`
+`;
 
 const Button = styled.button`
   padding: 1rem 2rem;
@@ -90,10 +87,10 @@ const Button = styled.button`
   border: 2px solid black;
   margin-right: 2rem;
   font-weight: 600;
-`
+`;
 
 const Info = styled.div`
   margin: 10rem;
-`
+`;
 
-export default Recipe
+export default Recipe;
